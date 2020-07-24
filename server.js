c6c9ea3e6f3119094cf79238ad41771aac65e155
Justin <ponthierjustin@gmail.com>
2020-07-24 19:26:45 -0400

    boilerplate info and folers completed

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 3000;
+
 const db = require("./models");
 
 const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
